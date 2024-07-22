e08a9517b9f0d2654475e264cdb271279791019e
Abhishek Kumar <abhi24033c@gmail.com>
2024-07-22 11:56:13 +0530

    ''

--- src/env.js
+++ src/env.js
@@ -3,3 +3,6 @@
 // export const backendUrl=`http://localhost:8000/api/v1/`;
 
 export const backendUrl=`https://loannode.onrender.com/api/v1/`
+
+
+// export const backendUrl=`https://parvnode.vercel.app/api/v1/`
